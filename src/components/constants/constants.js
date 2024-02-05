6e60aa8a47b3a0fccca5a1e0f2b0e5de68025ef6
Bedanta Gautom <bedantagautom2001@gmail.com>
2024-02-05 20:20:47 +0530

    first commit

---
+++ src/components/constants/constants.js
@@ -0,0 +1,4 @@
+export const chatGptToken = "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";
+export const chatGptUrl = "https://api.openai.com/v1/chat/completions";
+export const url = "https://api.surveysparrow.com";
+export const seperator = ":-:";
